ce316e051b11421c27cab919aa6c240d962c0431
Midhlaj Nechikkandan <112548839+midhlaj-nk@users.noreply.github.com>
2023-05-31 09:02:18 +0530

    Delete custom/pragtech_dental_management directory

--- custom/pragtech_dental_management/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import dental
-# import dental_lab
-# import dental_imaging
-from . import financing
-from . import stock_alert
-from . import dental_invoice
--- custom/pragtech_dental_management/report/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import  report_income_by_procedure
-from . import  report_patient_by_procedure
-from . import  report_income_by_doctor
-from . import  report_income_by_insurance_company
-from . import  account_invoice_report
-from . import claim_report
